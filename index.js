8f7732d7795a254fe3b0d4d2254718482ee24838
William Lee <wdjlee91@gmail.com>
2021-09-29 15:51:16 -0700

    postman tested time on query at about 130ms? not sure if this is best way to do this

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ app.use(express.json());
 // Routes
 
 // get one product just to see if things are working. this isn't final query
-app.get('/api/products/:product_id', (req, res) => {
-  const queryInstruction = `SELECT products.id, products.name, products.slogan, \
-    products.description, products.category, products.default_price, features.feature, \
-    features.value FROM products INNER JOIN features ON products.id = features.product_id \
-    WHERE products.id = ${req.params.product_id}`;
+app.get('/api/products/:product_id', async (req, res) => {
+  try {
+    const { product_id } = req.params;
+    const queryFeatures = 'SELECT features.feature, features.value FROM features WHERE features.product_id = $1';
+    const queryProducts = 'SELECT products.id, products.name, products.slogan, products.description, products.category, products.default_price FROM products where products.id = $1';
+    const featuresResults = await pool.query(queryFeatures, [product_id]);
+    const productsResults = await pool.query(queryProducts, [product_id]);
+    const productInfo = productsResults.rows[0];
 
-  pool.query(queryInstruction, (err, results) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      res.status(200).send(results);
-    }
-  });
+    res.status(200).send({ ...productInfo, features: featuresResults.rows });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 // Make sure connection exists with port
